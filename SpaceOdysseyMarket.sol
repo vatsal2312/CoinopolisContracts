43714241411a5bc621c3c0c11a608f2731d95227
Matt Cannon <mttcannon@gmail.com>
2021-05-31 11:45:02 -0500

    Update SpaceOdysseyMarket.sol

--- SpaceOdysseyMarket.sol
+++ SpaceOdysseyMarket.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.0;
 
 
 import "./SpaceOdyssey.sol";
+// imports are for remix
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
